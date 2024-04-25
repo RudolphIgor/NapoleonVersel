0cb08507e9155820f40d9a81c3170967c13b8662
RudolphIgor <106064657+RudolphIgor@users.noreply.github.com>
2024-04-25 14:54:47 +0700

    Comment

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,7 +4,7 @@ import Section2 from "../../components/Section2";
 import Section3 from "../../components/Section3";
 import Section4 from "../../components/Section4";
 import Section5 from "../../components/Section5";
-
+// Comment
 const Index = () => {
     return (
         <div>
