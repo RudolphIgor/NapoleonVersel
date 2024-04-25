2c3d423f15101300fec9ffeb30808e45b8e409af
RudolphIgor <106064657+RudolphIgor@users.noreply.github.com>
2024-04-25 14:24:59 +0700

    Test

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,6 +8,7 @@ import Section5 from "../../components/Section5";
 const Index = () => {
     return (
         <div>
+            TEST
             <Section1/>
             <Section2/>
             <Section3/>
