b6915e5821391b79b917597b6c38625742256b4f
RudolphIgor <106064657+RudolphIgor@users.noreply.github.com>
2024-04-25 14:46:49 +0700

    DEV

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,7 +8,7 @@ import Section5 from "../../components/Section5";
 const Index = () => {
     return (
         <div>
-            Branch dev
+            DEV
             <Section1/>
             <Section2/>
             <Section3/>
