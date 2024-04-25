0e5c71109370877b6c3905371ad2fe618199c9b2
RudolphIgor <106064657+RudolphIgor@users.noreply.github.com>
2024-04-25 15:03:34 +0700

    Delete text TEST

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,7 +8,6 @@ import Section5 from "../../components/Section5";
 const Index = () => {
     return (
         <div>
-            TEST
             <Section1/>
             <Section2/>
             <Section3/>
