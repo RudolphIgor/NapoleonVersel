e065031869a98efcb47f68297d017075ae23114f
RudolphIgor <106064657+RudolphIgor@users.noreply.github.com>
2024-05-21 09:23:39 +0700

    Добавлены еще 2 акции

--- src/data/promotions.js
+++ src/data/promotions.js
@@ -4,7 +4,7 @@ export const promotions = [
         title: "Акция на линолеум IDEAL",
         offer: "СТОП цена 645р/м2",
         image: "./img/promotions/kansas1.jpg",
-        text: "Текст описания акции, нужно придумать какой-то небольшой текс, о достоинствах данного продукта или акции",
+        text: "нужен текст",
         details: [
             {
                 id: 1,
@@ -20,25 +20,34 @@ export const promotions = [
     },
     {
         id: 2,
-        title: "Акция на ламинат",
-        offer: "Старая цена 895 новая 740р",
-        image: "./img/promotions/newberry.jpg",
-        text: "Текст описания акции, нужно придумать какой-то небольшой текс, о достоинствах данного продукта или акции",
+        title: "Скидка на ковровое покрытие",
+        offer: "1 июня до 1 июля",
+        image: "./img/promotions/kover.jpg",
+        text: "Купите напольное покрытие и получите скидку на ковры 20%",
         details: [
             {
                 id: 1,
-                title: "Collection first",
+                title: "Нужны детали акции и фото",
                 image: "./img/promotions/promodet1.jpg",
                 specifications: {
                     "Страна производитель": "Росиия",
                     "Класс износостойкости:": 32,
                     "Вид древесины":"Вид древесины:"
                 }
-            },
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: " Кварцвиниловая плитка (замковая)",
+        offer: "СТОП цена 1099₽ за кВ.м",
+        image: "./img/promotions/plitkaPvh.jpg",
+        text: "Нужен текст и фото",
+        details: [
             {
-                id: 2,
-                title: "Collection second",
-                image: "./img/promotions/promodet2.jpg",
+                id: 1,
+                title: "Нужны детали акции и фото",
+                image: "./img/promotions/promodet1.jpg",
                 specifications: {
                     "Страна производитель": "Росиия",
                     "Класс износостойкости:": 32,
