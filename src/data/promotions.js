7db383b60c5672cb24443d1925895290ea5878e8
RudolphIgor <106064657+RudolphIgor@users.noreply.github.com>
2024-05-24 08:57:48 +0700

    Добавлена 4 акция и переделаны три

--- src/data/promotions.js
+++ src/data/promotions.js
@@ -4,7 +4,7 @@ export const promotions = [
         title: "Акция на линолеум IDEAL",
         offer: "СТОП цена 645р/м2",
         image: "./img/promotions/kansas1.jpg",
-        text: "нужен текст",
+        text: "Линолеум полуммерческий Glory Kansas 1 –шириной 4м ",
         details: [
             {
                 id: 1,
@@ -22,36 +22,54 @@ export const promotions = [
         id: 2,
         title: "Скидка на ковровое покрытие",
         offer: "1 июня до 1 июля",
-        image: "./img/promotions/kover.jpg",
+        image: "./img/promotions/koverdetail.jpeg",
         text: "Купите напольное покрытие и получите скидку на ковры 20%",
         details: [
             {
                 id: 1,
-                title: "Нужны детали акции и фото",
-                image: "./img/promotions/promodet1.jpg",
+                title: "20% на Турецкие ковры",
+                image: "./img/promotions/kover.jpg",
                 specifications: {
-                    "Страна производитель": "Росиия",
-                    "Класс износостойкости:": 32,
-                    "Вид древесины":"Вид древесины:"
+                    "Размер": "2*3м",
                 }
             }
         ]
     },
     {
         id: 3,
-        title: " Кварцвиниловая плитка (замковая)",
+        title: "Акция кварц-винил от Crono Floor",
         offer: "СТОП цена 1099₽ за кВ.м",
-        image: "./img/promotions/plitkaPvh.jpg",
-        text: "Нужен текст и фото",
+        image: "./img/promotions/sps.jpg",
+        text: "Влагостойкий замковой SPS-ламинат  Дуб Колорадо. Акция действует на товар в наличии. Наличие уточняйте у менеджеров.",
         details: [
             {
                 id: 1,
-                title: "Нужны детали акции и фото",
-                image: "./img/promotions/promodet1.jpg",
+                title: "Наличие уточняйте у менеджеров",
+                image: "./img/promotions/spsdetails.jpeg",
                 specifications: {
-                    "Страна производитель": "Росиия",
-                    "Класс износостойкости:": 32,
-                    "Вид древесины":"Вид древесины:"
+                    "Тип соединения":"замковый",
+                    "Класс износостойкости:": 42,
+                    "Толщина":"3,5мм",
+                    "Толщина защитного слоя":"0,3мм",
+                }
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: "Акция ламинат 8мм",
+        offer: "СТОП цена 750р старая 997р.",
+        image: "./img/promotions/promo4.jpg",
+        text: "Ламинат Ютекс 33 класса, толщина 8мм,с фаской.",
+        details: [
+            {
+                id: 1,
+                title: "Наличие уточняйте у менеджеров",
+                image: "./img/promotions/detailPromo4.jpg",
+                specifications: {
+                    "Цвет":"Светло- серый оттенок",
+                    "Текстура": "Под дерево",
+                    "Класс износостойкости:": 33,
                 }
             }
         ]
